592525883367560950858664114df61cb00077d6
ht-ubuntu <htubuntu@htubuntu-VirtualBox.(none)>
2014-10-23 17:27:56 -0400

    announcements, admin, etc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+	@announcements = Announcement.all
+  end
+  def adminer
+  	current_user.update_attribute :admin, true
+  	render :inline => "You are now an admin."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ SEClub::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+  get '/adminme' => 'home#adminer'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
